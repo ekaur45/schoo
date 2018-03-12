5439be4e83f2451fc1794fec25d92dc614c20fe4
Waqas Ahmad <Waqas Ahmad@EKAUR45>
2018-03-12 15:10:59 +0500

    commit

--- WebApp/src/app/dashboard/dashboard.component.ts
+++ WebApp/src/app/dashboard/dashboard.component.ts
@@ -11,6 +11,9 @@ export class DashboardComponent implements OnInit {
   total_teachers;
   present_teachers;
   absent_teachers;
+  total_students;
+  present_students;
+  absent_students;
   numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
 
   constructor(private _service: CommonService) {
@@ -22,6 +25,9 @@ export class DashboardComponent implements OnInit {
     this.total_teachers = 30;
     this.absent_teachers = 4;
     this.present_teachers = this.total_teachers - this.absent_teachers;
+    this.total_students = 50;
+    this.absent_students = 12;
+    this.present_students = this.total_students - this.absent_students;
   }
   redirect(d) {
     location.href = "#/"+d;
